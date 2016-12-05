dbd5e9c1bd5d85504c1d71ae2cc5914a0ba2f2cc
Phil Johnston <support@mintplugins.com>
2016-12-05 15:46:33 -0500

    Simplified event edit screen

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -3,35 +3,35 @@
 * Filter function which inserts the shortcode into the content
 */
 function mp_events_the_content_filter( $content ){
-	
+
 	//Main query (page if page.php, archive if archive.php etc)
 	global $wp_query;
-	
+
 	//if this is a single page
 	if ( is_single() ){
-		
+
 		//If this is a post_type
 		if ( isset( $wp_query->query['post_type'] ) ) {
-		
+
 			//If that post type is mp_event
 			if( $wp_query->query['post_type'] == 'mp_event' ) {
-		
+
 				//If this post's content does NOT contains the shortcode to show event info already
 				if ( strpos( $content, '[mp_event]' ) === false  && !current_theme_supports( 'mp_events' )) {
-					
-					//attach the mp_event shortcode to this content	 
+
+					//attach the mp_event shortcode to this content
 					 return mp_events_single_event_shortcode() . $content;
-					 
+
 				}
 			}
 		}
-	
+
 	}
-	
+
 	//if not a single events page
-	return $content;	
-	
-		
+	return $content;
+
+
 }
 add_filter( 'the_content', 'mp_events_the_content_filter' );
 
@@ -45,9 +45,9 @@ add_filter( 'the_content', 'mp_events_the_content_filter' );
  * @return   The date string formatted.
  */
 function mp_events_get_mp_event_start_date( $post_id ){
-	
+
 	$date_format = get_option( 'date_format' );
-	
+
 	//If there is a date in the URL - get it from there.
 	if ( isset( $_GET['mp_event_start_date'] ) ){
 		//Get the date from the URL
@@ -58,7 +58,7 @@ function mp_events_get_mp_event_start_date( $post_id ){
 	else{
 		$event_start_date = mp_events_format_mp_event_date( get_post_meta( $post_id, 'event_start_date', true ) );
 	}
-	
+
 	return $event_start_date;
 }
 
@@ -72,9 +72,9 @@ function mp_events_get_mp_event_start_date( $post_id ){
  * @return   The date string formatted.
  */
 function mp_events_get_mp_event_end_date( $post_id ){
-	
+
 	$date_format = get_option( 'date_format' );
-	
+
 	//If there is a date in the URL - get it from there.
 	if ( isset( $_GET['mp_event_end_date'] ) ){
 		//Get the date from the URL
@@ -85,12 +85,12 @@ function mp_events_get_mp_event_end_date( $post_id ){
 	else{
 		$event_end_date = mp_events_format_mp_event_date( get_post_meta( $post_id, 'event_end_date', true ) );
 	}
-	
+
 	return $event_end_date;
 }
 
 /**
- * Format the MP Event Date 
+ * Format the MP Event Date
  *
  * @since    1.0.0
  * @link     http://mintplugins.com/doc/
@@ -99,10 +99,59 @@ function mp_events_get_mp_event_end_date( $post_id ){
  * @return   The date string formatted.
  */
 function mp_events_format_mp_event_date( $start_or_end_date_string ){
-	
+
 	$date_format = get_option( 'date_format' );
-	
+
 	$formatted_date = date($date_format, strtotime( $start_or_end_date_string ) );
-	
+
 	return $formatted_date;
 }
+
+/**
+ * Typically MP Events are viewed using something like EventGrid my Mint Plugins. The permalink, while useful for some, is not relevant for most.
+ * Since the basic permalink is not typically correct for event dates, we hide the permalink sample from the user here.
+ *
+ * @since    1.0.0
+ * @link     http://mintplugins.com/doc/
+ * @see      function_name()
+ * @param    $permalink_html
+ * @param    $post_id
+ * @param    $new_title
+ * @param    $new_slug
+ * @param    $post
+ * @return   The date string formatted.
+ */
+function mp_events_remove_sample_permalink_html( $permalink_html, $post_id, $new_title, $new_slug, $post ){
+
+	// Check the post type
+	if ( $post->post_type == 'mp_event' ){
+		return '';
+	}
+
+	return $permalink_html;
+
+}
+add_filter( 'get_sample_permalink_html', 'mp_events_remove_sample_permalink_html', 10, 5 );
+
+/**
+ * Typically MP Events are viewed using something like EventGrid my Mint Plugins. The permalink, while useful for some, is not relevant for most.
+ * Since the basic permalink is not typically correct for event dates, we hide the permalink sample from the user here.
+ *
+ * @since    1.0.0
+ * @link     http://mintplugins.com/doc/
+ * @see      function_name()
+ * @param    $permalink_html
+ * @param    $post_id
+ * @param    $new_title
+ * @param    $new_slug
+ * @param    $post
+ * @return   The date string formatted.
+ */
+function mp_events_remove_view_event( $wp_admin_bar ) {
+	global $post;
+
+	if ( $post->post_type == 'mp_event' ){
+		$wp_admin_bar->remove_node( 'view' );
+	}
+}
+add_action( 'admin_bar_menu', 'mp_events_remove_view_event', 999 );
